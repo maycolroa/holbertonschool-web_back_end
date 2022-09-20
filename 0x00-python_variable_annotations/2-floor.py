c67cc611b306a5450aebd50db8ec878b145c17e4
maycolroa <lehallo@hotmail.com>
2022-09-19 20:59:30 -0500

    0x00. Python - Anotaciones de variables  19/09/22 0.02

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function floor which takes a float n"""
+
+
+def floor (n: float) -> int:
+    """returns the floor of the float"""
+    return int(n)
