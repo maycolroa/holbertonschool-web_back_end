7695363a1a2f53f4f0c9fb4525286f28e7649571
maycolroa <lehallo@hotmail.com>
2022-09-20 22:41:33 -0500

    0x01-python_async_function  20/09/22 0.01

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""asynchronous function and import modules"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n - make delays as much as n value"""
+    wList = []
+    for i in range(n):
+        wList.append(await wait_random(max_delay))
+
+    return sorted(wList)
+    
