10b7eb1a4c428c64c914b95cd05d6378e624eec5
maycolroa <lehallo@hotmail.com>
2022-09-21 21:39:15 -0500

    0x02. Python - Async Comprehension  21/09/22 0.01

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Import async_generator from the previous task and then write a coroutine called async_comprehension"""
+"""Import async_generator from the previous task"""
 import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
