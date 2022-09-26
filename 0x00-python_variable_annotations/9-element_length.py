92973f11d2dd4cf1c6959f491f9c812206c60b8e
maycolroa <lehallo@hotmail.com>
2022-09-25 19:13:20 -0500

    0x00 25/09/22 0.09

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Annotate the below functions parameters and return values with the appropriate types"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return a list of tuples with sequence and integer"""
+    return [(i, len(i)) for i in lst]
