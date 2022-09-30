c383b80e826576a9014976b2ee05e6043074ea9d
maycolroa <lehallo@hotmail.com>
2022-09-30 14:00:43 -0500

    0x05-personal_data 30/09/22 0.00

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Write a function called filter_datum that returns the log message obfuscated"""
+from typing import List
+import re
+import logging
+import os
+import mysql.connector
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """class RedactingFormatter"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def filter_datum(fields: List[str], redaction: str,
+                message: str, separator: str) -> str:
+    """filter_datum"""
+    for field in fields:
+        message = re.sub(r'(?<={}=)[^{}]+'.format(field, separator),
+                        redaction, message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """get_logger"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """get_db"""
+    connector = mysql.connector.connect(
+        user=os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.environ.get('PERSONAL_DATA_DB_NAME')
+    )
+    return connector
+
+
+def main():
+    """main"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+    for row in cursor:
+        logger.log(logging.INFO, row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
