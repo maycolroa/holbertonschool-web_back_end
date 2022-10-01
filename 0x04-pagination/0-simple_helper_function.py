185689f3a9a545f15f2ed85fc96d381007808e44
maycolroa <lehallo@hotmail.com>
2022-10-01 11:38:09 -0500

    0x04. Pagination 01/10/22 0.00

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""""""
+"""function"""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """"""
+    """function"""
     upper_limit = (page * page_size) - page_size
     lower_limit = page * page_size
     indexes_range = (upper_limit, lower_limit)
