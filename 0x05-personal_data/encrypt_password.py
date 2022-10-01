9418dfd1621558c52dedb486b9cfe8f15725a776
maycolroa <lehallo@hotmail.com>
2022-09-30 22:11:17 -0500

    0x05-personal_data 30/09/22 0.03

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """Implement a main function that takes no arguments and returns nothing."""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """functiooon password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """functiooon valid"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
