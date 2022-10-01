6f990116d1592994c312c0bc443a81e0cd4b5f5b
maycolroa <lehallo@hotmail.com>
2022-10-01 11:32:25 -0500

    0x04. Pagination 01/10/22 0.00

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""""""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """"""
+    upper_limit = (page * page_size) - page_size
+    lower_limit = page * page_size
+    indexes_range = (upper_limit, lower_limit)
+    return indexes_range
+    
