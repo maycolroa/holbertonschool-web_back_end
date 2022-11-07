26391725a2f6fedecd96356d45ae35b41eea3790
maycolroa <lehallo@hotmail.com>
2022-11-06 22:40:09 -0500

    0x0C. MySQL advanced 06/11/22 0.12

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,22 @@
+-- cript that creates a stored procedure ComputeAverageWeightedScoreForUsers
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+	UPDATE users
+	SET average_score = (
+		SELECT SUM(corrections.score * projects.weight) /
+		SUM(projects.weight
+		)
+	FROM corrections
+	INNER JOIN projects
+	ON projects.id = corrections.project_id
+	WHERE corrections.user_id = users.id);
+END
+
+$$
+
+DELIMITER ;
