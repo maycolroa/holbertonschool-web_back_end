f57134061b00961c75be5bb49d4775b9b8de3b67
maycolroa <lehallo@hotmail.com>
2022-11-09 14:30:25 -0500

    0x0E. ES6 Basics 09/11/22 0.01

--- 0x0E-ES6_basic/main.js
+++ 0x0E-ES6_basic/main.js
@@ -1,3 +1,5 @@
-import { taskFirst, taskNext } from './0-constants.js';
+import taskBlock from './1-block-scoped.js';
 
-console.log(`${taskFirst()} ${taskNext()}`);
+console.log(taskBlock(true));
+console.log(taskBlock(false));
+
