0e7d5df30105e4bfa68829d63c4450e9fa556227
maycolroa <lehallo@hotmail.com>
2022-11-10 09:43:25 -0500

    0x0E. ES6 Basics 10/11/22 0.00

--- 0x0E-ES6_basic/0-constants.js
+++ 0x0E-ES6_basic/0-constants.js
@@ -1,10 +1,13 @@
+// Modify function
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
 }
+
 export function getLast() {
     return ' is okay';
 }
+
 export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
