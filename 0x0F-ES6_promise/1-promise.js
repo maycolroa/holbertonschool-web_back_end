0e1a64b25e5084d48a33e0f3fdec8f4899b63c3d
maycolroa <lehallo@hotmail.com>
2022-11-15 03:11:13 -0500

    0x0F. Promesas ES6 14/11/22 0.01

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve ({
+      resolve({
         status: 200,
-        body: 'Success'
+        body: 'Success',
       });
     } else {
-      reject(Error("The fake API is not working currently"))
+      reject(Error('The fake API is not working currently'));
     }
-  })
+  });
 }
