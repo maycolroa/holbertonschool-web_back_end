57d6799f7b55b765f991baa1c5975d645cb27304
maycolroa <lehallo@hotmail.com>
2022-11-28 10:48:02 -0500

    0x12. NodeJS Basics 28/11/22 0.01

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("readable", () => {
-  const name = process.stdin.read();
-  if (name) {
-    process.stdout.write(`Your name is: ${name}`);
+process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const student = process.stdin.read();
+  if (student !== null) {
+    process.stdout.write(`Your name is: ${student}`);
   }
 });
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
